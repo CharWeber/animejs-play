0a84b181fdddf00d5dd098cb34954aaf00217fd8
Charlie Weber <charlestweber@gmail.com>
2021-09-05 13:10:54 -0700

    playing with box animations

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import anime from 'animejs/lib/anime.es';
+
+let boxanime = anime({
+  targets:'#box',
+  duration:3000,
+  translateX:[0,600],
+  loop: true,
+  easing: 'steps(10)',
+  direction: 'alternate',
+})
